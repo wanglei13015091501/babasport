a6cd3805074a8c8a1aff6d3577c9db504e5208b1
lx <lixu@itcast.cn>
2017-08-17 12:16:41 +0800

    地

--- babasport-common/src/main/java/com/itheima/common/Demo.java
+++ babasport-common/src/main/java/com/itheima/common/Demo.java
@@ -5,6 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("02");
 		System.out.println("00");
+		System.out.println("00");
+		System.out.println("00");
 	}
 
 }
