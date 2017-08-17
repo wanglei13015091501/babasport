4fac1064f83453e4ee7044821d5d3abf95918ef8
lx <lixu@itcast.cn>
2017-08-17 12:19:08 +0800

    要

--- babasport-common/src/main/java/com/itheima/common/Demo.java
+++ babasport-common/src/main/java/com/itheima/common/Demo.java
@@ -4,6 +4,8 @@ public class Demo {
 	
 	public static void main(String[] args) {
 		System.out.println("02");
+		System.out.println("02");
+		System.out.println("02");
 		System.out.println("00");
 		System.out.println("00");
 		System.out.println("00");
