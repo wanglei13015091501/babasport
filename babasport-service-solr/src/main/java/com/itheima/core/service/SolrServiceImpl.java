e2778afe449c29d92d44e257c56636736892d357
twelvescore <twelvescores@163.com>
2019-08-29 17:37:04 +0800

    create project

--- babasport-service-solr/src/main/java/com/itheima/core/service/SolrServiceImpl.java
+++ babasport-service-solr/src/main/java/com/itheima/core/service/SolrServiceImpl.java
@@ -1,7 +1,10 @@
 package com.itheima.core.service;
 
 import com.itheima.common.page.Pagination;
+import com.itheima.core.pojo.product.ProductQuery;
 import com.itheima.core.service.solr.SolrService;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,7 +20,21 @@ public class SolrServiceImpl implements SolrService {
     @Autowired
     private SolrServer solrServer;
 
-    public Pagination selectPaginationByQuery(){
+    public Pagination selectPaginationByQuery(Integer pageNo,String keyword,Long brandId,String price){
+        //商品对象Query
+        ProductQuery productQuery = new ProductQuery();
+        //当前页
+        productQuery.setPageNo(Pagination.cpn(pageNo));
+        //每页数
+        productQuery.setPageSize(8);
+        SolrQuery solrQuery = new SolrQuery();
+
+        //条件
+        if(StringUtils.isNotBlank(keyword)){
+            //关键词
+        }
+
+
         return new Pagination();
     }
 }
